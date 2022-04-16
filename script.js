51a1f468b083d11ac2b0d457c75dfe051958a9e2
Josh <joshua.frederick.anderson@gmail.com>
2022-04-15 23:51:25 -0400

    added rating functionality

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-let rate = document.querySelector('numberbutton');
 
-const getRating = (rating) => {
-   
-}
+const rateState = () => {
+    let notRated = document.getElementById('rateStartState');
+    let rated = document.getElementById('thankYouSection');
+    if (rated.style.display !== "none") {
+        rated.style.display = "block"; 
+        notRated.style.display ="none";
+    } else {
+        rated.style.display = "none";
+    }
+};
+
+const
+    display = document.querySelector(".text"),
+    buttons = document.querySelectorAll(".numberbutton");
+
+buttons.forEach(button => button.addEventListener("click", function() {
+    display.value = button.value;
+    display.innerHTML = `You selected ${display.value} out of 5`;
+}));
